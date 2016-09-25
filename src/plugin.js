76d20fa7dc8c6ad49b26b20167bfa2f0cf2accc8
Rogerio Chaves <rogeriochaves@thoughtworks.com>
2016-09-24 22:26:49 -0300

    Add webpack plugin for preloading all requirejs plugins before the initial module and use the result for fullfil the requires

---
+++ src/plugin.js
@@ -0,0 +1,78 @@
+let ReplaceSource = require('webpack-sources').ReplaceSource;
+let RawSource = require('webpack-sources').RawSource;
+
+class RequirejsPluginsLoaderPlugin {
+  apply (compiler) {
+    addPluginToCompiler(compiler);
+  }
+};
+
+const addPluginToCompiler = (compiler) => {
+  compiler.plugin('compilation', (compilation) => {
+    compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+      replaceChunks(compilation, chunks);
+      callback();
+    });
+  });
+};
+
+const replaceChunks = (compilation, chunks) => chunks.forEach((chunk) => {
+  chunk.files.forEach((file) => {
+    const original = compilation.assets[file].source();
+    const parsed = parse(original);
+    const replacedSource = new ReplaceSource(new RawSource(parsed), compilation.assets[file]);
+    compilation.assets[file] = replacedSource;
+  });
+});
+
+const parse = (file) => {
+  const pluginRequires = findPluginRequires(file);
+  const plugins = pluginRequires ? parsePluginRequires(pluginRequires) : [];
+  const patchedRequires = patchRequires(file);
+  const patchedInitialLoad = patchInitialLoad(patchedRequires, plugins);
+  return patchedInitialLoad;
+};
+
+const findPluginRequires = (file) => file.match(new RegExp(pluginsRegex, 'g'));
+
+const pluginsRegex = '__webpack_require__\\((.*?)\\) \\/\\/ requirejs_plugin\\|(.*?)\\|';
+
+const patchRequires = (file) =>
+      file.replace(new RegExp(pluginsRegex, 'g'), `__webpack_require__.requirejs_plugin['$1!$2']`);
+
+const parsePluginRequires = (plugins) =>
+      plugins.reduce(parsePluginRequire, []);
+
+const parsePluginRequire = (plugins, file) => {
+  const parts = file.match(new RegExp(pluginsRegex));
+  return [...plugins, {
+    webpackRequire: parts[1],
+    args: parts[2]
+  }]
+};
+
+const initialRequire = '__webpack_require__(0);';
+
+const patchInitialLoad = (file, plugins) =>
+      addPluginsLoad(file.replace(`return ${initialRequire}`,
+         `__webpack_require__.requirejs_plugin = {};
+          return ${initialRequire}`), plugins);
+
+const addPluginsLoad = (file, plugins) =>
+      plugins.reduce(addPluginLoad, file);
+
+const addPluginLoad = (file, plugin) =>
+      file.replace(initialRequire,
+         `__webpack_require__(${plugin.webpackRequire}).load('${plugin.args}', {}, function (result) {
+            __webpack_require__.requirejs_plugin['${plugin.webpackRequire}!${plugin.args}'] = result;
+            ${initialRequire}
+          }, {});`);
+
+module.exports = {
+  RequirejsPluginsLoaderPlugin,
+  patchRequires,
+  findPluginRequires,
+  parsePluginRequires,
+  patchInitialLoad,
+  parse
+};
---
+++ test/plugin.spec.js
@@ -0,0 +1,120 @@
+let expect = require('chai').expect;
+let plugin = require('../src/plugin');
+let jsBeautify = require('js-beautify').js_beautify;
+let beautify = (data) =>
+  jsBeautify(data, {max_preserve_newlines: 1})
+let moduleSyntaxFixtures = require('./moduleSyntaxFixtures');
+
+describe('plugin', () => {
+  ;
+  const bundledFileFixture = beautify(
+    `/******/ 	// Load entry module and return exports
+    /******/ 	return __webpack_require__(0);
+    /* 0 */
+    /***/ function(module, exports, __webpack_require__) {
+      var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
+        __webpack_require__(1)
+      ], __WEBPACK_AMD_DEFINE_RESULT__ = function (secondDependency) {
+        console.log('result', secondDependency.pluginResult);
+      }.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+
+      /***/ },
+    /* 1 */
+    /***/ function(module, exports, __webpack_require__) {
+
+      var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
+        __webpack_require__(2) // requirejs_plugin|someArg|
+      ], __WEBPACK_AMD_DEFINE_RESULT__ = function (loadedPlugin) {
+        return {
+          pluginResult: loadedPlugin.result
+        };
+      }.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+    }
+  `);
+
+  it('patches the requires, replacing with requirejs_plugin require', () => {
+    expect(plugin.patchRequires(bundledFileFixture)).not.to.contain(
+      '__webpack_require__(2) // requirejs_plugin|someArg|'
+    );
+    expect(plugin.patchRequires(bundledFileFixture)).to.contain(
+      "__webpack_require__.requirejs_plugin['2!someArg']"
+    );
+  });
+
+  it('finds plugin requires', () => {
+    expect(plugin.findPluginRequires(bundledFileFixture)).to.deep.equal([
+      '__webpack_require__(2) // requirejs_plugin|someArg|'
+    ]);
+  });
+
+  it('parses the plugin requires', () => {
+    const pluginRequires = [
+      '__webpack_require__(2) // requirejs_plugin|someArg|',
+      '__webpack_require__(3) // requirejs_plugin|*|'
+    ];
+
+    expect(plugin.parsePluginRequires(pluginRequires)).to.deep.equal([{
+      webpackRequire: '2',
+      args: 'someArg'
+    }, {
+      webpackRequire: '3',
+      args: '*'
+    }]);
+  });
+
+  it('patches the initial load with all the plugins', () => {
+    const plugins = [{
+      webpackRequire: '2',
+      args: 'someArg'
+    }, {
+      webpackRequire: '3',
+      args: '*'
+    }];
+    initialLoad = 'return __webpack_require__(0);';
+
+    expect(beautify(plugin.patchInitialLoad(initialLoad, plugins))).to.equal(beautify(
+      `__webpack_require__.requirejs_plugin = {};
+      return __webpack_require__(2).load('someArg', {}, function (result) {
+        __webpack_require__.requirejs_plugin['2!someArg'] = result;
+        __webpack_require__(3).load('*', {}, function (result) {
+          __webpack_require__.requirejs_plugin['3!*'] = result;
+          __webpack_require__(0);
+        }, {});
+      }, {});
+      `));
+  });
+
+  it('parses a whole file', () => {
+    const parsed = plugin.parse(bundledFileFixture);
+    expect(beautify(parsed)).to.equal(beautify(
+      `/******/ 	// Load entry module and return exports
+      /******/ 	__webpack_require__.requirejs_plugin = {};
+      return __webpack_require__(2).load('someArg', {}, function (result) {
+        __webpack_require__.requirejs_plugin['2!someArg'] = result;
+        __webpack_require__(0);
+      }, {});
+      /* 0 */
+      /***/ function(module, exports, __webpack_require__) {
+        var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
+          __webpack_require__(1)
+        ], __WEBPACK_AMD_DEFINE_RESULT__ = function (secondDependency) {
+          console.log('result', secondDependency.pluginResult);
+        }.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+
+        /***/ },
+      /* 1 */
+      /***/ function(module, exports, __webpack_require__) {
+
+        var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
+          __webpack_require__.requirejs_plugin['2!someArg']
+        ], __WEBPACK_AMD_DEFINE_RESULT__ = function (loadedPlugin) {
+          return {
+            pluginResult: loadedPlugin.result
+          };
+        }.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+      }
+      `));
+  });
+});
